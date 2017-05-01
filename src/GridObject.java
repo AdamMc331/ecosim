e2f689209b66a0ef973259d38d9815c99a781c3d
AdamMc331 <amcneilly331@gmail.com>
2017-04-30 21:26:30 -0400

    adding comments for GridObject.

--- src/GridObject.java
+++ src/GridObject.java
@@ -44,24 +44,27 @@ abstract class GridObject implements Comparable<GridObject>{
 
     //This is here because it's the same for wolves and sheep
     public int findNullSpace(ArrayList<GridObject> options) {
-        //TODO: Instead of this hacky for loop, you can use https://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html#indexOf(java.lang.Object)
-        // indexOf will give you the first index that an item exists, or -1 if it doesn't. So, using this awesome arraylist method,
-        // this thing becomes a one liner.
-        return options.indexOf(null);
-
-//        if (options.contains(null)) {
-//            ArrayList<Integer> nulls = new ArrayList<>(0);
-//            for (int i = 0; i < options.size(); i ++) {
-//                if (options.get(i) == null) {
-//                    nulls.add(i);
-//                }
-//            }
-//            Collections.shuffle(nulls);
-//            return nulls.get(0);
-//        } else {
-//            //Give up and don't move
-//            return -1;
-//        }
+        if (options.contains(null)) {
+            //TODO: Super minor performance optomization, but something to keep in mind because this bit me in an interview once.
+            // When you say new ArrayList<>(0). You're instantiating it with a size 0. So, once you add the first item,
+            // the system has to go reallocate more memory. A trick you could use is that, since you know the size of
+            // nulls can't be larger than the size of options, you can start with that value so you never have to
+            // worry about the system reassigning more memory.
+            ArrayList<Integer> nulls = new ArrayList<>(options.size());
+            for (int i = 0; i < options.size(); i ++) {
+                if (options.get(i) == null) {
+                    nulls.add(i);
+                }
+            }
+            Collections.shuffle(nulls);
+            //TODO: I would normally say be wary of Null Pointer Exceptions here, but since you already checked that
+            // options contains null, you're guaranteed that this will have at least one item.
+            // So this is fine, but a note to be aware of in the future.
+            return nulls.get(0);
+        } else {
+            //Give up and don't move
+            return -1;
+        }
     }
 
     abstract int findTarget(ArrayList<GridObject> options);
