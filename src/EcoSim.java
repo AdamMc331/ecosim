409b2e23db8f4f643d9adb3aa1b33f5d0d66c655
AdamMc331 <amcneilly331@gmail.com>
2017-04-30 22:20:01 -0400

    Commenting on EcoSim.java

--- src/EcoSim.java
+++ src/EcoSim.java
@@ -10,26 +10,33 @@ import java.util.ArrayList;
 public class EcoSim {
 
     //Important constants
-    private static int GRID_SIZE = 15;
-    private static int ITERATIONS = 1000;
-    private static double GROWTH_RATE = 5.4; //Chance a new plant will spawn that turn (Can be greater than 1)
+    //TODO: As mentioned in DisplayGrid.java, a number of these fields are only used once, so they can probably be defined as a local variable
+    // instead of at the class level. This is less important here than in the other file, since these are static, though.
+    // one way around the intellij warnings is to label them `private static final`. By adding in the final keyword,
+    // you're making it so that they can't be changed, so you don't have to worry about the race condition I mentioned
+    // in my DisplayGrid comments.
+    private static final int GRID_SIZE = 15;
+    private static final int ITERATIONS = 1000;
+    private static final double GROWTH_RATE = 5.4; //Chance a new plant will spawn that turn (Can be greater than 1)
     //Example: 20.4 guarantees 20 plants per turn, plus 40% chance of a 21st plant
-    private static int TICK_LENGTH = 200; //Milliseconds per grid refresh
+    private static final int TICK_LENGTH = 200; //Milliseconds per grid refresh
 
     //World densities. Not guaranteed but they'll be close.
-    private static double WORLD_FILL_DENSITY = 0.5;
+    private static final double WORLD_FILL_DENSITY = 0.5;
     //WOLF_DENSITY + SHEEP_DENSITY + PLANT_DENSITY = 1
-    private static double WOLF_DENSITY = 0.05;
-    private static double SHEEP_DENSITY = 0.9;
+    private static final double WOLF_DENSITY = 0.05;
+    private static final double SHEEP_DENSITY = 0.9;
     //We don't need to declare PLANT_DENSITY because we get it algebraically
 
     //Mating values
     public static int MIN_MATE_HEALTH = 50;
     public static int BABY_HEALTH = 40;
 
-    private static double STRUGGLE_CHANCE = 0.3; //Chance a wolf will be unable to attack a weaker sheep
+    private static final double STRUGGLE_CHANCE = 0.3; //Chance a wolf will be unable to attack a weaker sheep
 
-    private static int currentIter;
+    //TODO: Keeping a static index of your current position is a bad idea. So many race conditions.
+    // Instead, I've added it as a parameter to the methods that need it.
+    // private static int currentIter;
 
     public static void main(String[] args) throws Exception {
         GridObject[][] map = new GridObject[GRID_SIZE][GRID_SIZE];
@@ -45,13 +52,26 @@ public class EcoSim {
                     //Choose the GridObject we're going to spawn
                     double randomType = Math.random();
                     //Spawn a GridObject
+//                    if (randomType <= WOLF_DENSITY) {
+//                        map[y][x] = new Wolf();
+//                    } else if (randomType <= WOLF_DENSITY + SHEEP_DENSITY) {
+//                        map[y][x] = new Sheep();
+//                    } else {
+//                        map[y][x] = new Plant();
+//                    }
+
+                    //TODO: What you have is fine, but I would typically go with the same route I did in GridObject
+                    // where I get the object I want, and then do the assignment. This is just personal preference
+                    GridObject gridObject = null;
                     if (randomType <= WOLF_DENSITY) {
-                        map[y][x] = new Wolf();
+                        gridObject = new Wolf();
                     } else if (randomType <= WOLF_DENSITY + SHEEP_DENSITY) {
-                        map[y][x] = new Sheep();
+                        gridObject = new Sheep();
                     } else {
-                        map[y][x] = new Plant();
+                        gridObject = new Plant();
                     }
+
+                    map[y][x] = gridObject;
                 }
             }
         }
@@ -62,33 +82,30 @@ public class EcoSim {
 
         //Run the specified number of iterations
         for(int i = 0; i < ITERATIONS; i++) {
-            currentIter = i;
 
             double plantSpawn = GROWTH_RATE;
             do {
                 //Figure out if we want to spawn a plant this turn
-                if (Math.random() <= plantSpawn) {
-                    //Spawn a plant
-                    if (EcoSim.hasSpaceLeft(map)) {
-                        GridObject newPlantSpot;
-                        int newY;
-                        int newX;
-                        do { //Statistically, this should never be an infinite loop
-                            newY = (int) (Math.random() * GRID_SIZE);
-                            newX = (int) (Math.random() * GRID_SIZE);
-                            newPlantSpot = map[newY][newX];
-                        } while (newPlantSpot != null);
+                //TODO: You have two if statements here, combine them into one for simplicity
+                if (Math.random() <= plantSpawn && EcoSim.hasSpaceLeft(map)) {
+                    GridObject newPlantSpot;
+                    int newY;
+                    int newX;
+                    do { //Statistically, this should never be an infinite loop
+                        newY = (int) (Math.random() * GRID_SIZE);
+                        newX = (int) (Math.random() * GRID_SIZE);
+                        newPlantSpot = map[newY][newX];
+                    } while (newPlantSpot != null);
 
-                        //Let it grow
-                        map[newY][newX] = new Plant();
-                    }
+                    //Let it grow
+                    map[newY][newX] = new Plant();
                 }
                 plantSpawn -= 1;
             } while(plantSpawn > 1);
 
             for (int y = 0; y < GRID_SIZE; y++) {
                 for (int x = 0; x < GRID_SIZE; x++) {
-                    EcoSim.updateObject(x, y, map);
+                    EcoSim.updateObject(x, y, map, i);
                 }
             }
             grid.refresh();
@@ -98,12 +115,15 @@ public class EcoSim {
         }
     }
 
-    private static void updateObject(int x, int y, GridObject[][] map) {
+    private static void updateObject(int x, int y, GridObject[][] map, int currentPosition) {
         GridObject object = map[y][x];
         //Make sure the object is an animal we haven't moved yet
-        if (object != null && !(object instanceof Plant) && object.getLastUpdated() < currentIter) {
+        if (object != null && !(object instanceof Plant) && object.getLastUpdated() < currentPosition) {
 
             //Create the ArrayList instances to be passed to the object
+            //TODO: Notice what I said in GridObject about instantiating these as 0. In this case, I think the
+            // largest size might be 3? Not sure. You /can/ leave it at 0 and it's not bad at all, just commenting
+            // on performance for academic sake.
             ArrayList<GridObject> options = new ArrayList<>(0);
             ArrayList<int[]> optionCoords = new ArrayList<>(0);
 
@@ -245,7 +265,7 @@ public class EcoSim {
         }
 
         //Flag the object as updated
-        if (object != null) { object.setLastUpdated(currentIter); }
+        if (object != null) { object.setLastUpdated(currentPosition); }
     }
 
     private static boolean hasSpaceLeft(GridObject[][] map) {
